892a436f9f8ade719acc8888e9a5fbd5cd536ac2
Stanislav M <phoenix0597@yandex.ru>
2025-02-06 05:55:53 +0300

    Move main app to app dir

---
+++ app/main.py
@@ -0,0 +1,14 @@
+import uvicorn
+from fastapi import FastAPI
+
+from app.api.routes import router
+from app.core.config import settings
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+)
+app.include_router(router, prefix=settings.API_V1_STR)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
